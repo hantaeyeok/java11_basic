2a517c3cd6b877fd6f0a0c86b1c88df9b59a7cd7
hantaeyeok <gksxodur1@gmail.com>
2024-03-05 17:36:47 +0900

    java_11_test12

---
+++ test13/src/sec1/InnerClassExam.java
@@ -0,0 +1,10 @@
+package sec1;
+//내부 클래스 : Inner Class : 클래스 내부에 선언한 클래스 
+public class InnerClassExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
